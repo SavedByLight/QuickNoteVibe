5063a46715457483f3e594a4e5e4d2a9558918b5
Jamie Falconer <mrfluffyoven@gmail.com>
2026-09-10 12:20:21 +0100

    remove old file

--- app/src/main/java/com/app/quicknotesvibe/App.kt
+++
@@ -1,8 +0,0 @@
-package com.app.quicknotesvibe
-
-import android.app.Application
-
-class App : Application() {
-    companion object { lateinit var context: App }
-    override fun onCreate() { super.onCreate(); context = this }
-}
